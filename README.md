b69a4021e9edddf14b25f12e9a71b479c7671079
Samip-Nepal <samipnepal5@email.com>
2024-02-12 21:45:31 +0545

    Pratice qn 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Array
-# Js-fourth-chapter
+# Array
+1)pratice qn 1
+q)for a given array with of student [85,97,44,37,76,60].find the average marks of the entire class.
--- first.js
+++ first.js
@@ -1,18 +1,27 @@
-let marks=[20,34,35,37,54,70,80,60];
-//for loop
-for(let i=0;i<marks.length;i++)
-{
-    console.log(marks[i]);
+//pratice qn 1
+let marks=[85,97,44,37,76,60];
+let sum=0;
+for (let i=0;i<marks.length;i++){
+    sum=sum+marks[i];
 }
-let books=["Science","Math","English","Nepali"];
-for(let book of books){
-    console.log(book.toUpperCase());
-}
+let avg=sum/marks.length;
+console.log(`The averge marks of total student is= ${avg}`);
 
 
 
 
 
+
+// let marks=[20,34,35,37,54,70,80,60];
+// //for loop
+// for(let i=0;i<marks.length;i++)
+// {
+//     console.log(marks[i]);
+// }
+// let books=["Science","Math","English","Nepali"];
+// for(let book of books){
+//     console.log(book.toUpperCase());
+// }
 // console.log(marks);
 // console.log(marks[2]);
 // console.log(marks[5]);//undefine
