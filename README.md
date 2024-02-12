342bca10f8ea8284ac54d8f21bbc0707d8f0fd8b
Samip-Nepal <samipnepal5@email.com>
2024-02-12 22:04:49 +0545

    Pratice qn 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Array
 1)pratice qn 1
 q)for a given array with of student [85,97,44,37,76,60].find the average marks of the entire class.
+q)for the given array with price of 5 item [250,645,300,900,50]
+all items have an offer of 105 OFF on them.change the array to store final price after applying offer.
--- first.js
+++ first.js
@@ -1,17 +1,20 @@
-//pratice qn 1
-let marks=[85,97,44,37,76,60];
-let sum=0;
-for (let i=0;i<marks.length;i++){
-    sum=sum+marks[i];
+//pratice qn 2
+let items=[250,645,300,900,50];
+for(i=0;i<items.length;i++)
+{
+    let value=items[i]/10;
+    let off=items[i]-value;
+  items[i]=off;
+console.log(`The value of  item after 10% off ${items[i]}`);
 }
-let avg=sum/marks.length;
-console.log(`The averge marks of total student is= ${avg}`);
-
-
-
-
-
-
+//pratice qn 1
+// let marks=[85,97,44,37,76,60];
+// let sum=0;
+// for (let i=0;i<marks.length;i++){
+//     sum=sum+marks[i];
+// }
+// let avg=sum/marks.length;
+// console.log(`The averge marks of total student is= ${avg}`);
 // let marks=[20,34,35,37,54,70,80,60];
 // //for loop
 // for(let i=0;i<marks.length;i++)
