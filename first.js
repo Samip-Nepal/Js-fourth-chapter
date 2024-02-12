33ab9f0d45d8c2313d8e01459f2ba5a23aec6265
Samip-Nepal <samipnepal5@email.com>
2024-02-12 21:21:36 +0545

    Array indices

--- first.js
+++ first.js
@@ -1,5 +1,9 @@
 let marks=[20,34,35,37,54];
 console.log(marks);
+console.log(marks[2]);
+console.log(marks[5]);//undefine
 let books=["Science","Math","English"];
 console.log(books);
 console.log(marks.length);
+books[1]="Samip";
+console.log(books);
