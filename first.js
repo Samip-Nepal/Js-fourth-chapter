dca5740affc8e4de03c66fc7624dde8ba22b8477
Samip-Nepal <samipnepal5@email.com>
2024-02-12 21:35:58 +0545

    printing the value using loop (for ,for of)

--- first.js
+++ first.js
@@ -1,9 +1,23 @@
-let marks=[20,34,35,37,54];
-console.log(marks);
-console.log(marks[2]);
-console.log(marks[5]);//undefine
-let books=["Science","Math","English"];
-console.log(books);
-console.log(marks.length);
-books[1]="Samip";
-console.log(books);
+let marks=[20,34,35,37,54,70,80,60];
+//for loop
+for(let i=0;i<marks.length;i++)
+{
+    console.log(marks[i]);
+}
+let books=["Science","Math","English","Nepali"];
+for(let book of books){
+    console.log(book.toUpperCase());
+}
+
+
+
+
+
+// console.log(marks);
+// console.log(marks[2]);
+// console.log(marks[5]);//undefine
+// let books=["Science","Math","English"];
+// console.log(books);
+// console.log(marks.length);
+// books[1]="Samip";
+// console.log(books);
